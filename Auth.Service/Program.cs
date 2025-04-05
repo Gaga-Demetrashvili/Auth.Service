18d0c1ef465398009a0a84a2e7ce06111bf650db
Gaga Demetrashvili <gagademetrashvili@credo.ge>
2025-04-06 01:00:09 +0400

    Add code for JWT Authentication

--- Auth.Service/Program.cs
+++ Auth.Service/Program.cs
@@ -1,10 +1,15 @@
+using Auth.Service.Endpoints;
 using Auth.Service.Infrastructure.Data.EntityFramework;
+using Auth.Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSqlServerDatastore(builder.Configuration);
+builder.Services.RegisterTokenService(builder.Configuration);
 
 var app = builder.Build();
+
+app.RegisterEndpoints();
 
 if (app.Environment.IsDevelopment())
 {
